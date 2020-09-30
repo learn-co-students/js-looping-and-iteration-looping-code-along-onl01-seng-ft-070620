1b371c6c2a895a592f00094098bb542769d720ee
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-30 13:39:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let new_array = [];
+
+function writeCards(name_array, event_name) {
+  for (let i = 0; i < name_array.length; i++) {
+    new_array.push(`Thank you, ${name_array[i]}, for the wonderful ${event_name} gift!`);
+  }
+  return new_array;
+}
+
+function countDown(num) {
+  while (num > -1) {
+    console.log(num);
+    num--;
+  }
+}

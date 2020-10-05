38fbd4914450ee5c7567bd34a347af37fcb69f69
Ethan <eleetyson@gmail.com>
2020-10-05 15:23:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// function uses a for loop to return an array of thank you messages
+function writeCards(strings, event) {
+  let message = [];
+  for (let i = 0; i < strings.length; i++) {
+    message.push(`Thank you, ${strings[i]}, for the wonderful ${event} gift!`);
+  }
+  return message;
+}
+
+// function uses a while loop to count down to 0
+function countDown(start) {
+  while (start >= 0) {
+    console.log(start);
+    start --;
+  }
+}
